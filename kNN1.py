9c8682604a03ac78225de7adc237bbcec9e3856d
yangyuyou <1084274425@qq.com>
2020-07-17 14:07:37 +0800

    first commit

---
+++ kNN1.py
@@ -0,0 +1,21 @@
+from numpy import*
+import operator
+
+def createDataSet():
+    groups=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels=['A','A','B','B']
+    return groups,labels
+def classify0(inX,dataSet,labels,k):
+    dataSetSize=dataSet.shape[0]
+    diffMat=tile(inX,(dataSetSize,1))-dataSet
+    sqDiffMat=diffMat**2
+    sqDistances=sqDiffMat.sum(axis=1)
+    distances=sqDistances**0.5
+    sortedDistIndices=distances.argsort()
+    classCount={}
+    for i in range(k):
+        votelLabel=labels[sortedDistIndices[i]]
+        classCount[votelLabel]=classCount.get(votelLabel,0)+1
+    sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+    
